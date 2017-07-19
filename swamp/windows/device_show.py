cc1a1f8b822188d9eb8f18b723be8c3f96e397a6
wanghongxu <wang19930902@gmail.com>
2017-07-19 23:09:37 +0800

    Window change
    Change-Id: I55e3a0d86b6fe35f617a0e417a1fdd96fe4ac9fc

--- swamp/windows/device_show.py
+++ swamp/windows/device_show.py
@@ -3,8 +3,8 @@ import json
 import matplotlib.pyplot as plt
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtGui import QMessageBox
-from matplotlib.backends.backend_qt4agg import \
-    FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as \
+    FigureCanvas
 
 from swamp.windows.device_select import Window as SelectWindow
 from swamp import log
@@ -23,28 +23,52 @@ class MainWindow(QtGui.QMainWindow):
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
 
-        btn_layout = QtGui.QVBoxLayout()
+        # label grid
+        label_grid = QtGui.QGridLayout()
+        name_lable = QtGui.QLabel("Name:")
+        device_name = QtGui.QLabel("")
+        label_grid.addWidget(name_lable, 0, 0)
+        label_grid.addWidget(device_name, 0, 1)
+        self.device_name = device_name
+
+        health_label = QtGui.QLabel("Health:")
+        device_health = QtGui.QLabel("")
+        label_grid.addWidget(health_label, 1, 0)
+        label_grid.addWidget(device_health, 1, 1)
+        self.device_health = device_health
+
+        # button grid begin
+        btn_grid = QtGui.QGridLayout()
 
         select_btn = QtGui.QPushButton('Select Device')
         select_btn.clicked.connect(self.select_btn_click)
-        btn_layout.addWidget(select_btn)
+        btn_grid.addWidget(select_btn, 0, 0)
 
         check_btn = QtGui.QPushButton('Check Device')
         check_btn.clicked.connect(self.check_btn_click)
-        btn_layout.addWidget(check_btn)
+        btn_grid.addWidget(check_btn, 0, 1)
 
         history_btn = QtGui.QPushButton('Historical Data')
         history_btn.clicked.connect(self.history_btn_click)
-        btn_layout.addWidget(history_btn)
+        btn_grid.addWidget(history_btn, 1, 0)
 
         exit_btn = QtGui.QPushButton('Exit')
         exit_btn.clicked.connect(self.close)
-        btn_layout.addWidget(exit_btn)
+        btn_grid.addWidget(exit_btn,  1, 1)
 
+        # right layout
+        right_layout = QtGui.QVBoxLayout()
+        right_layout.addStretch(1)
+        right_layout.addLayout(label_grid)
+        right_layout.addStretch(1)
+        right_layout.addLayout(btn_grid)
+
+        # window layout
         layout = QtGui.QHBoxLayout()
+        layout.addStretch(2)
         layout.addWidget(self.canvas)
         layout.addStretch(1)
-        layout.addLayout(btn_layout)
+        layout.addLayout(right_layout)
 
         self.setCentralWidget(QtGui.QWidget())
         self.centralWidget().setLayout(layout)
@@ -90,4 +114,5 @@ class MainWindow(QtGui.QMainWindow):
     @device.setter
     def device(self, value):
         self._device = value
+        self.device_name.setText(value.name)
         self.statusBar().showMessage("Device: %s" % value.name)
