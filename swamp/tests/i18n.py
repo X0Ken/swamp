19a5825982b0bc0e7727b54d46e63b13d61a9b6f
wanghongxu <wang19930902@gmail.com>
2017-08-30 22:42:07 +0800

    Test i18n
    Change-Id: I0f4be15dd57e0e321f7a1bdc4eebba89e24747ad

---
+++ swamp/tests/i18n.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+"""
+This is a test for i18n.
+
+First:
+    xgettext -k_ -o i18n.po_en i18n.py
+    xgettext -k_ -o i18n.po_zh i18n.py
+Next:
+    msgfmt -o ./locale/en_US/LC_MESSAGES/i18n.mo i18n.po_en
+    msgfmt -o ./locale/zh_CN/LC_MESSAGES/i18n.mo i18n.po_zh
+"""
+
+import gettext
+
+locale_path = './locale/'
+# gettext.install('internation', locale_path) # 这条语句会将_()函数自动放到python的内置命名空间中
+zh_trans = gettext.translation('i18n', locale_path, languages=['zh_CN'])
+en_trans = gettext.translation('i18n', locale_path, languages=['en_US'])
+
+print "----中文版本----"
+zh_trans.install()
+print _("Hello world!")
+print _("Python is a good Language.")
+
+print "----英文版本----"
+en_trans.install()
+print _("Hello world!")
+print _("Python is a good Language.")
